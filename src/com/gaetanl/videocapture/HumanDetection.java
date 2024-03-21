ca68a7c3041ec3c41cecacc07fd631ebb73a1bb3
Gaetan LEU <gleu-presta@cora.fr>
2024-03-21 16:36:23 +0100

    Code cleaning

--- src/com/gaetanl/videocapture/HumanDetection.java
+++ src/com/gaetanl/videocapture/HumanDetection.java
@@ -22,64 +22,129 @@ import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.Videoio;
 
+/**
+ * Detection of human face/body in video feed.
+ *
+ * @author Gaetan L.
+ */
 public class HumanDetection {
+	/**
+	 * Path to folder containing Haar Cascade Classifier files, relative to
+	 * project root folder.
+	 */
 	private final static String RESOURCES_PATH = "resources/";
+
+	/**
+	 * Path to folder used to save signaling frames, relative to project root
+	 * folder.
+	 */
 	private final static String DET_FRAME_PATH = "frame/";
+
+
+
+	/**
+	 * Name of the Haar Cascade Classifier file detecting face. 
+	 */
 	private final static String HAAR_FR_FACE_1 = "haarcascade_frontalface_default.xml";
+
+	/**
+	 * Name of the Haar Cascade Classifier file detecting full body. 
+	 */
 	private final static String HAAR_FULL_BODY = "haarcascade_fullbody.xml";
+
+	/**
+	 * Names of the Haar Cascade Classifier file detecting upper body. 
+	 */
 	private final static String HAAR_UPPR_BODY = "haarcascade_upperbody.xml";
+
+	/**
+	 * Name of the widow displaying video stream.
+	 */
 	private final static String CAPTURE_WINDOW = "Human detection";
-	private final static int FRAMERATE = 100;
+
+
+
+	/**
+	 * Time between each capture in the capture loop, in milliseconds.
+	 */
+	private final static int FRAMERATE         =  100;
+
+	/**
+	 * Maximum time between two detections for them to be considered
+	 * continuous.
+	 */
+	private final static int CTIME_BREAK       =  200;
+
+	/**
+	 * Minimum continuous detection time before signaling.
+	 */
+	private final static int MIN_CTIME_SIGNAL  = 1000;
+
+	/**
+	 * Minimum time between each signaling calls.
+	 */
+	private final static int MIN_TIME_RESIGNAL = 2000;
+
+
+
+	/**
+	 * Used for logging.
+	 */
 	private final static DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+
+	/**
+	 * Used for file naming.
+	 */
 	private final static DateTimeFormatter FILE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd_hhmmssSSS");
 
-	/*
-	 * Maximum amount of time between two detections for them to be considered
-	 * continuous, in milliseconds.
-	 */
-	private final static int MAX_TIME_FOR_CONTINUOUS = 200;
 
-	/*
-	 * Number of continuous detections.
-	 */
-	private static int continuousDetections;
 
-	/*
+	/**
 	 * Beginning of the current continuous detection.
 	 */
 	private static LocalDateTime continuousDetectionStart;
 
-	/*
+	/**
 	 * Time of the last detection.
 	 */
 	private static LocalDateTime lastDetectionTime;
 
-	/*
-	 * Minimum continuous detection time before signaling a person is really
-	 * present, in milliseconds.
-	 */
-	private final static int MIN_CONTINUOUS_TIME_BEFORE_SIGNALING = 1000;
-
-	/*
-	 * Minimum time between signaling calls, in milliseconds.
-	 */
-	private final static int MIN_TIME_BETWEEN_SIGNALING = 2000;
-
-	/*
+	/**
 	 * Time of the last signaling.
 	 */
 	private static LocalDateTime lastSignalingTime;
 	
 
-	// Has to be loaded before instantiating frames
+	/*
+	 * Has to be loaded before instantiating frames.
+	 */
 	static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
 
+	/**
+	 * User input scanner.
+	 */
 	private static final Scanner SCANNER = new Scanner(System.in);
-	private static final VideoCapture CAPTURE = new VideoCapture(Videoio.CAP_DSHOW);;
+
+	/**
+	 * OpenCV class in charge of video capture.
+	 */
+	private static final VideoCapture CAPTURE = new VideoCapture(Videoio.CAP_DSHOW);
+
+	/**
+	 * OpenCV class in charge of storing the values of a frame.
+	 */
 	private static final Mat FRAME = new Mat();
+
+	/**
+	 * OpenCV class in charge of transforming a frame to greyscale.
+	 */
 	private static final Mat GRAY_FRAME = new Mat();
+
+	/**
+	 * OpenCV class in charge of displaying detected objects.
+	 */
 	private static final MatOfRect DETECTION_FRAME = new MatOfRect();
 
     public static void main(String[] args) throws InterruptedException {
@@ -111,7 +176,7 @@ public class HumanDetection {
 	        			throw new InputMismatchException();
 	            }
 
-	            loasClassifier(filePath, humanDetector);
+	            loadClassifier(filePath, humanDetector);
             }
             catch (InputMismatchException | FileNotFoundException | IllegalArgumentException e) {
                 System.out.println(e.getMessage() == null ? "Invalid choice" : String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage()));
@@ -122,23 +187,47 @@ public class HumanDetection {
         }
     }
 
-    private static void loasClassifier(final String path, final CascadeClassifier humanDetector) throws FileNotFoundException, IllegalArgumentException {
+    /**
+     * Loads the Haar Cascade Classifier corresponding to the specified file
+     * into the CascadeClassifier object.
+     *
+     * @param  path -------------------- path of the XML file containing the
+     *                                   Haar Cascade Classifier
+     * @param  humanDetector ----------- CascadeClassifier object into which
+     *                                   the file is loaded
+     *
+     * @throws FileNotFoundException --- if the file is not found
+     * @throws IllegalArgumentException  if the CascadeClassifier object
+     *                                   couldn't load the file 
+     */
+    private static void loadClassifier(final String path, final CascadeClassifier humanDetector) throws FileNotFoundException, IllegalArgumentException {
         if (!(new File(path)).exists()) throw new FileNotFoundException(String.format("File %s not found", path));
         if (!humanDetector.load(path))  throw new IllegalArgumentException(String.format("Couldn't load CascadeClassifier with %s", path));
         System.out.println(String.format("Loaded CascadeClassifier with %s", path));
     }
 
+    /**
+     * Loop doing the video capture and detection. HighGui.waitKey(FRAMERATE)
+     * insures a minimum duration (FRAMERATE value) between two iterations.
+     *
+     * @param  humanDetector  the CascadeClassifier used for detection
+     */
     private static void captureLoop(final CascadeClassifier humanDetector) {
-        // Displaying monitor
+        // Window for displaying results
         HighGui.namedWindow(CAPTURE_WINDOW, HighGui.WINDOW_AUTOSIZE);
+
+
 
         // Initialize video capture from camera
         if (!CAPTURE.isOpened()) {
             System.err.println("Video capture is not open");
             System.err.println("Check that camera is not in use in another program or running instance of Java");
 
+            // Back to the choice input loop
             return;
         }
+
+
 
         // Detection loop
         while (CAPTURE.read(FRAME)) {
@@ -146,21 +235,22 @@ public class HumanDetection {
             Imgproc.cvtColor(FRAME, GRAY_FRAME, Imgproc.COLOR_BGR2GRAY);
             Imgproc.equalizeHist(GRAY_FRAME, GRAY_FRAME);
 
-            // Human detection
+            // Detecting and putting every detection into DETECTION_FRAME
             humanDetector.detectMultiScale(GRAY_FRAME, DETECTION_FRAME, 1.1, 2, 0, new Size(30, 30), new Size());
+            Rect[] detections = DETECTION_FRAME.toArray();
 
-            // Highlighting detected objects
-            for (Rect rect: DETECTION_FRAME.toArray()) {
+            // Highlighting each detected object in FRAME
+            for (Rect rect: detections) {
                 Imgproc.rectangle(FRAME, rect.tl(), rect.br(), new Scalar(0, 255, 0), 2);
+            }
 
-                long timeDifference = 0;
+
+
+            // Actions in case of detection(s)
+            if (detections.length > 0) {
+            	// (Re)initializing loop variables
                 LocalDateTime time = LocalDateTime.now();
-
-                if (lastDetectionTime != null) {
-	                timeDifference = lastDetectionTime.until(time, ChronoUnit.MILLIS);
-                }
-
-            	lastDetectionTime = time;
+                long timeDifference = 0;
             	long continuousDetectionTime = 0;
             	boolean resetContinuous = false;
             	boolean signaling = false;
@@ -168,24 +258,27 @@ public class HumanDetection {
             	boolean minBeforeSignaling = false;
             	boolean savedImage = false;
 
-            	resetContinuous = (continuousDetections > 1) && (timeDifference > MAX_TIME_FOR_CONTINUOUS);
+                if (lastDetectionTime != null) timeDifference = lastDetectionTime.until(time, ChronoUnit.MILLIS);
+            	lastDetectionTime = time;
+
+            	resetContinuous = timeDifference > CTIME_BREAK;
                 if (resetContinuous) {
                 	// Too long since last detection, reseting lastDetectionTime
-                	continuousDetections = 1;
                 	continuousDetectionStart = time;
                 }
                 else {
-                	continuousDetections++;
                 	if (continuousDetectionStart == null) continuousDetectionStart = time;
 
                 	continuousDetectionTime = continuousDetectionStart.until(time, ChronoUnit.MILLIS);
-                	minContinuous = continuousDetectionTime > MIN_CONTINUOUS_TIME_BEFORE_SIGNALING;
-                	minBeforeSignaling = (lastSignalingTime == null) || lastSignalingTime.until(time, ChronoUnit.MILLIS) > MIN_TIME_BETWEEN_SIGNALING;
+                	minContinuous = continuousDetectionTime > MIN_CTIME_SIGNAL;
+                	minBeforeSignaling = (lastSignalingTime == null) || lastSignalingTime.until(time, ChronoUnit.MILLIS) > MIN_TIME_RESIGNAL;
                 	signaling = minContinuous && minBeforeSignaling;
                     if (signaling) {
                     	lastSignalingTime = time;
 
-                    	// Saving frame to image
+                    	// TODO: Put actual signaling code here
+
+                    	// Saving detected frame to image
                     	if (DET_FRAME_PATH != null) {
 	                    	String imagePath = String.format("%sframe_%s.jpg", DET_FRAME_PATH, FILE_TIME_FORMATTER.format(time));
 	                    	savedImage = Imgcodecs.imwrite(imagePath, FRAME);
@@ -193,6 +286,9 @@ public class HumanDetection {
                     }
                 }
 
+
+
+                // Logging information
                 StringBuilder messageSb = new StringBuilder();
                 String level = "[INFO]";
                 if (signaling) {
@@ -213,13 +309,12 @@ public class HumanDetection {
                 	else if (!minBeforeSignaling) messageSb.append("Insufficient delay since last signaling");
                 }
 
-                System.out.println(String.format(" %-7s  TIME: %s  |  EVNT: %9s  |  STRT: %s  |  CONT: %6dms  |  ITER: %6d  |  DIFF: %6dms  |  SGNL: %9s  |  MESG: %s",
+                System.out.println(String.format(" %-7s  TIME: %s  |  EVNT: %9s  |  STRT: %s  |  CONT: %6dms  |  DIFF: %6dms  |  SGNL: %9s  |  MESG: %s",
                 		level,
                 		TIME_FORMATTER.format(time),
                 		"Detection",
                 		(continuousDetectionStart == null ? "            " : TIME_FORMATTER.format(continuousDetectionStart)),
                 		(continuousDetectionStart == null ? "            " : continuousDetectionStart.until(time, ChronoUnit.MILLIS)),
-                		continuousDetections,
                 		timeDifference,
                 		signaling ? "Signaling" : "Inhibited",
                 		messageSb.toString()));
@@ -235,7 +330,12 @@ public class HumanDetection {
         }
     }
 
-    private static void exit(final int exitCode) throws InterruptedException {
+    /**
+     * Exits the program after releasing the various OpenCV resources.
+     *
+     * @param  exitCode  Code returned to System.exit
+     */
+    private static void exit(final int exitCode) {
     	// Releasing resources
     	if (SCANNER != null) SCANNER.close();
         System.out.println("Scanner closed");
